8c8a1d8cf597f3454004d106e3be13e2f0b26f4c
unknown <dpartho5616@gmail.com>
2024-03-17 00:20:06 +0600

    setup portfolio

---
+++ src/assets/pages/main/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Header/Header.css';
+
+const Header = () => {
+    return (
+        <div className='partho'>
+            <h1>Partho das</h1>
+        </div>
+    );
+};
+
+export default Header;
