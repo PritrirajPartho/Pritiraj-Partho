16ff26dbd66b4c1b40d826eb53bc83997fce872d
unknown <dpartho5616@gmail.com>
2024-03-18 01:29:16 +0600

    projects added

--- src/assets/pages/main/Home.jsx
+++ src/assets/pages/main/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Overview from './Overview/Overview';
 import '../main/Home.css';
 import About from './About/About';
+import Projects from './Projects/Projects';
 
 const Home = () => {
     return (
         <div className='home'>
             <Overview></Overview>
             <About></About>
-            {/* <About></About>
             <Projects></Projects>
+            {/* <About></About>
             <Contact></Contact> */}
             hello home
         </div>
--- src/assets/pages/main/Projects/Projects.jsx
+++ src/assets/pages/main/Projects/Projects.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
 import '../Projects/Projects.css';
+import project1 from '../../../../../public/summer-camp-school.png'
+import project2 from '../../../../../public/web-developers.png'
+import project3 from '../../../../../public/startling.png'
 
 const Projects = () => {
     return (
-        <div>
-            
-        </div>
+     <>
+             <h1 className='mt-2 mb-10 text-center text-3xl text-violet'>--Projects--</h1>
+        <section className='projects'>
+            {/*projets one sparkle sports academy*/}
+            <div>
+                <div>
+                     <img src={project1} alt="" />
+                </div>
+                <div>
+                    <h1>Sparkle Sports Academy</h1>
+                    <p>It is Full stack website project. A student or visitor easily can sign up or sign in with the email and Google or Github system.Every Student can see our popular Instructors and Courses list for the best user experience on this website.</p>
+                    <div>
+                        <button ><a href="https://summer-camp-school-356df.web.app/" target='_blank'>Live site</a></button>
+                        <button ><a href="https://github.com/PritrirajPartho/sparkle-sports-academy" target='_blank'>Github repo</a></button>
+                    </div>
+                </div>
+            </div>
+            {/* project two web developers 360*/}
+            <div>
+                <div>
+                     <img src={project2} alt="" />
+                </div>
+                <div>
+                    <h1>Sparkle Sports Academy</h1>
+                    <p>It is Full stack website project. A student or visitor easily can sign up or sign in with the email and Google or Github system.Every Student can see our popular Instructors and Courses list for the best user experience on this website.</p>
+                    <div>
+                        <button ><a href="https://summer-camp-school-356df.web.app/" target='_blank'>Live site</a></button>
+                        <button ><a href="https://github.com/PritrirajPartho/sparkle-sports-academy" target='_blank'>Github repo</a></button>
+                    </div>
+                </div>
+
+            </div>
+            {/*projects three starting justice*/}
+            <div>
+                <div>
+                     <img src={project3} alt="" />
+                </div>
+                <div>
+                    <h1>Justice--law website</h1>
+                    <p>It is generall html5,css3 and bootstrap based website making for project practice. pixel perfect website making task from my training company.</p>
+                    <div>
+                        <button ><a href="https://startling-platypus-c6f46f.netlify.app/" target='_blank'>Live site</a></button>
+                        <button ><a href="https://startling-platypus-c6f46f.netlify.app/" target='_blank'>Github repo</a></button>
+                    </div>
+                </div>
+            </div>
+        </section>
+     </>
     );
 };
 
