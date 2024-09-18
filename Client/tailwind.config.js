ac4fff28d6e0f3ac0e8b398cd1426ac0d5fdfcc3
Pritiraj Partho <dpartho5616@gmail.com>
2024-09-18 10:10:40 +0600

    organize tailwind.config file

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,22 +5,49 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'violet' : 'rgb(139 92 246)',
-      'sky': 'rgb(96 165 250)',
-      'black': '#020617',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'violet' : 'rgb(139 92 246)',
+        'sky': 'rgb(96 165 250)',
+        'black': '#020617',
+      },
+      fontSize: {
+        xs: '0.2',
+        sm: '0.3vw',
+        base: '0.5vw',
+        lg: '1vw',
+        xl: '1.5vw',
+        '2xl': '2vw',
+        '3xl': '3vw',
+        '4xl': '4vw',
+        '5xl': '5vw',
+        '6xl': '6vw',
+      },
+      screens: {
+        'sm': '400px',
+        // => @media (min-width: 400px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        'lg': '992px',
+        // => @media (min-width: 992px) { ... }
+      },
+      fontFamily:{
+        garamond: ['EB Garamond', 'serif'],
+        poppins:  ['Poppins', 'sans-serif'],
+        agdasima: ['Agdasima','sans-serif'],
+        agufina:  ['Aguafina Script','cursive']
+      }
     },
-    extend: {},
   },
   plugins: [require("daisyui")],
 }
